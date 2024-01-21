2a3b4c958145e6a74ff0b725ff2330d10c894af8
TufanKeser <021270079tufan@gmail.com>
2024-01-22 00:28:45 +0300

    weekOneAssignment One

--- TutortAcademy/Program.cs
+++ TutortAcademy/Program.cs
@@ -1,117 +1,77 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System;
-using System.Runtime.InteropServices;
-using System.Text;
-using TutortAcademy.WeekOneIntroductionToProgramming;
+﻿using TutortAcademy.WeekOneIntroductionToProgramming;
+using WeekOneIntroduction.TestData;
+
+AssignmentOne assignmentOne = new AssignmentOne();
+TestDataForAssingments testDataForAssingments = new TestDataForAssingments();
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("\nQuestion One is started\n");
+Console.ForegroundColor = ConsoleColor.Red;
+assignmentOne.QuestionOne_findMaxOfThreeElements(testDataForAssingments.questionOneTempData);
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("\nQuestion One is finished\n");
 
 
-LectureOne lectureOne = new LectureOne();
-LectureTwo lectureTwo = new LectureTwo();
-AssignmentOne lectureOneAssignment = new AssignmentOne();
-
-/*
-
-int?[] arr = { 4, 1, 5, 6, 7, 8, 10, 2, -10, -11, 8, null, null, null, null, null };
-
-lectureOne.isElementPresent(arr,10,8);
-lectureOne.isElementPresentSecond(arr,10,8);
-
-Console.WriteLine("LectureOneIsFınıshed");
-Console.WriteLine("LectureOneIsFınıshed");
+Console.WriteLine("\nQuestion Two is started\n");
+Console.ForegroundColor = ConsoleColor.Red;
+assignmentOne.QuestionTwo_findMinOfThreeElements(testDataForAssingments.questionTwoTempData);
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("\nQuestion Two is finished\n");
 
 
+Console.WriteLine("\nQuestion Three is started\n");
+Console.ForegroundColor = ConsoleColor.Red;
+assignmentOne.QuestionThree_findTheMidElementsOfThreeElements(testDataForAssingments.questionThreeTempData);
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("\nQuestion Three is finished\n");
 
 
-*/
-//lectureTwo.pascalTriangeTufanSolution();
+Console.WriteLine("\nQuestion Four is started\n");
+Console.ForegroundColor = ConsoleColor.Red;
+assignmentOne.QuestionFour_nTimesNumberTriangleWriter(testDataForAssingments.questionFourInput);
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("\nQuestion Four is finished\n");
 
-//lectureTwo.printPatternOne();
-
-
-int[] arg = { 1, 2, 3};
-//lectureOneAssignment.findMaxOfThreeElements(arg);
-//lectureOneAssignment.findMinOfThreeElements(arg);
-
-
-//lectureOneAssignment.findTheMidElementsOfThreeElements(arg);
-
-char[] arr = { 'a', 'b', 'c','d','e','f','g','h'};
-
-// part 1 'd','e','f','g','h' => h g f e d
-// part 2 a b c => reverse c b a
-
-// expectedresult c b a h g f e d => d e f g h a b c
-
-//lectureFour
-/*void rotateFrom(char[] arr, int from)
-{
-    reverse(arr, from, arr.Length - 1);
-    reverse(arr, 0, from - 1);
-    reverse(arr, 0, arr.Length - 1);
-}
+Console.WriteLine("\nQuestion Five is started\n");
+Console.ForegroundColor = ConsoleColor.Red;
+assignmentOne.QuestionFive_nTimesNumberTriangleWriterReverse(testDataForAssingments.questionFiveInput);
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("\nQuestion Five is finished\n");
 
 
-##*/
-
-//problem 3
-//Run length encoding "abbbcddddeeefaaaa" => ab3cd5e3fa4
-
-
-//Solve Yourself
+Console.WriteLine("\nQuestion Six is started\n");
+Console.ForegroundColor = ConsoleColor.Red;
+assignmentOne.QuestionSix_findOddElements(testDataForAssingments.questionSixTempData);
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("\nQuestion Six is finished\n");
 
 
-//problem 4
-//Run length type 4.
-//"abbbcddddeeefaaaa" => "a1b3c1d5e3f1a4" output will never be longer then input
-
-
-// "abcd" => "a1b1c1d1" wrong input
-// "aaabcd" => a3b1c1d1 invalid input
-
-//  Solve this problem without additional space or solve i inplace.
+Console.WriteLine("\nQuestion Six solution two is started\n");
+Console.ForegroundColor = ConsoleColor.Red;
+assignmentOne.QuestionSix_findOddElementsSolTwo(testDataForAssingments.questionSixTempData);
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("\nQuestion Six solution two is finished\n");
 
 
 
+Console.WriteLine("\nQuestion Seven is started\n");
+Console.ForegroundColor = ConsoleColor.Red;
+assignmentOne.QuestionSeven_sumOfTheGivenArray(testDataForAssingments.questionSevenTempData);
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("\nQuestion Seven is finished\n");
 
 
+Console.WriteLine("\nQuestion Eight evenCase is started\n");
+Console.ForegroundColor = ConsoleColor.Red;
+assignmentOne.QuestionEight_findFirstEvenNumber(testDataForAssingments.questionEightTempDataForFirstEvenNumber);
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("\nQuestion Seven evenCase is finished\n");
 
 
+Console.WriteLine("\nQuestion Eight -1 case is started\n");
+Console.ForegroundColor = ConsoleColor.Red;
+assignmentOne.QuestionEight_findFirstEvenNumber(testDataForAssingments.questionEightTempDataForMınusOne);
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("\nQuestion Seven -1 case is finished\n");
 
 
-
-
-
-
-
-//benefit of compression: a => a1 => -1
-// aaaaa => a5 => 3
-
-// [a,b,c,d,e,e,e,e,e,e]
-// [a,b,c,d,e,7,null,null,null,null,null]
-
-// [null,null,null,null,null,a,b,c,d,e,7]
-
-//[a,1, b1, c,1, d,1, e,7] ;
-
-// step-1
-// 
-
-///### Next followup question 
-///"aabbbbbb3334555555" => "a2b6321375"
-
-// Change this value to adjust the number of rows
-
-// Loop through each row
-//lectureOneAssignment.nTimesNumberTriangleWriter(10);
-
-int[] array = { 1, 5, 6, 4, 3, 2, 8 };
-
-int[] sumArray = { 1, 2, 6, 3, 5 };
-
-int[] oddArray = { 1, 3, 5, 7 }; 
-
-//lectureOneAssignment.findOddElements(array);
-//lectureOneAssignment.sumOfTheGivenArray(sumArray);
-
-
-lectureOneAssignment.findFirstEvenNumber(oddArray);
---
+++ TutortAcademy/TestData/TestDataForAssingments.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeekOneIntroduction.TestData
+{
+    public class TestDataForAssingments
+    {
+       public int[] questionOneTempData = { 1, 2, 3 };
+       public int[] questionTwoTempData = { 1, 2, 3 };
+       public int[] questionThreeTempData = { 1, 2, 3, 4, 5, 6, 7, 8 };
+       public int questionFourInput = 5;
+       public int questionFiveInput = 5;
+       public int[] questionSixTempData = { 1, 5, 6, 4, 3, 2, 8 };
+       public int[] questionSevenTempData = { 1, 2, 6, 3, 5 };
+       public int[] questionEightTempDataForMınusOne = { 1, 3, 5, 7 };
+       public int[] questionEightTempDataForFirstEvenNumber = { 1, 2, 6, 3, 5 };
+    }
+}
